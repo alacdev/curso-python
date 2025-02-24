f4d29986d44e16df6c219f760bfaa70cb0e128c4
Hugo Alén Acuña <hugoaalen@gmail.com>
2025-02-24 19:19:33 +0100

    Primer commit del curso de astro de midudev

---
+++ 01_basic/exercises.py
@@ -0,0 +1,59 @@
+###
+# exercises.py
+# Ejercicios para practicar los conceptos aprendidos en las lecciones.
+###
+
+print("\nEjercicio 1: Imprimir mensajes")
+print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
+
+print("Hugo")
+print("Vigo")
+
+print("--------------")
+
+print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+a = 15
+b = 3.14159
+c = "Hola mundo"
+d = True
+e = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print("--------------")
+
+print("\nEjercicio 3: Casting de tipos")
+print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+
+float(int("12345"))
+int(3.99)
+
+print("--------------")
+
+print("\nEjercicio 4: Variables")
+print("Crea variables para tu nombre, edad y altura.")
+print("Usa f-strings para imprimir una presentación.")
+
+# "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
+nombre = "Hugo"
+edad = 21
+altura = 1.96
+
+print(f"Hola! Me llamo {nombre}, tengo {edad} años y mido {altura} metros")
+
+print("--------------")
+
+print("\nEjercicio 5: Números")
+print("1. Crea una variable con el número PI (sin asignar una variable)")
+print("2. Redondea el número con round()")
+print("3. Haz la división entera entre el número que te salió y el número 2")
+print("4. El resultado debería ser 1")
+
+resultado = round(3.14159)
+print(resultado // 2)
